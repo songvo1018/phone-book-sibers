02054321d654207d11dfa8d3d822b80d9723ef49
Leha <song.vo@yandex.ru>
2020-12-01 12:17:44 +0700

    misprint

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 
-reimport "./ContactList.css"
+import "./ContactList.css"
 import GroupedContactsByName from "./GroupedByName"
 import FavoriteContacts from "./FavoriteContacts"
 import SearchContacts from "./SearchContacts"
